052b8a964a5f16faea538d8b5b207804529af462
Natanael Leite Silva <natanxboy@gmail.com>
2024-08-23 15:07:23 -0300

    add: Pessoa model

--- MeuApp/models.py
+++ MeuApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    cpf = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return f"<[{self.id}] - {self.nome} {self.sobrenome}, {self.idade}>"
